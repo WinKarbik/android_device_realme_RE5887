4f36ad38b374cbbafddae1a0bd698d2f5729c831
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-08-09 09:23:39 +0200

    RE5887: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: realme Model: RMX3581 Script version: 3.0.0

---
+++ omni_RE5887.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from RE5887 device
+$(call inherit-product, device/realme/RE5887/device.mk)
+
+PRODUCT_DEVICE := RE5887
+PRODUCT_NAME := omni_RE5887
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3581
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3581_Natv-user 11 RP1A.201005.001 8442 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX3581/RE5887:11/RP1A.201005.001/1677147083000:user/release-keys
