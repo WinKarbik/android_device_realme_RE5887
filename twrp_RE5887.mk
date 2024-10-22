3b6e3abb18787a71ff2beb2bea439586ec428249
WinKarbik <karbainovs@mail.ru>
2024-10-22 22:13:50 +0800

    RE5887: de OMNI-fy

--- twrp_RE5887.mk
+++ twrp_RE5887.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RE5887 device
 $(call inherit-product, device/realme/RE5887/device.mk)
 
 PRODUCT_DEVICE := RE5887
-PRODUCT_NAME := omni_RE5887
+PRODUCT_NAME := twrp_RE5887
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3581
 PRODUCT_MANUFACTURER := realme
