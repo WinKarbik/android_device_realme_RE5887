2a48a36e5337af4d547677c1313a9845cde610e3
Sergey Karbainov <karbainovs@mail.ru>
2024-10-23 15:39:08 +0800

    RE5887: Include gsi keys
    To match stock boot.img

--- twrp_RE5887.mk
+++ twrp_RE5887.mk
@@ -8,6 +8,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
