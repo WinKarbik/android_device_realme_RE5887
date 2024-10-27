49adbe44f68d7de4e31a43543ef057a72c190a59
WinKarbik <karbainovs@mail.ru>
2024-10-28 01:22:27 +0800

    RE5887: Update device tree
    * Use crypto blobs from RMX3511 (Still don't work)
    * Start android boot hal(sprd boot hal still not ready, slot change don't work, but twrp bootable)
    * Add recovery.tmpfsdata.fstab

--- device.mk
+++ device.mk
@@ -17,6 +17,17 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl \
