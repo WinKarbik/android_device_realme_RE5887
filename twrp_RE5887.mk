9524c2da06c3561cf5601d900840ea0e479e03fc
Sergey Karbainov <karbainovs@mail.ru>
2024-10-29 20:36:29 +0800

    Update twrp_RE5887.mk

--- twrp_RE5887.mk
+++ twrp_RE5887.mk
@@ -9,7 +9,8 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
