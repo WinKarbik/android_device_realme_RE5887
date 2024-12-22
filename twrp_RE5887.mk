e1e5de491371a3d19833264bc506b9462d62a782
Sergey Karbainov <karbainovs@mail.ru>
2024-12-22 12:18:36 +1000

    RE5887: GO-ify

--- twrp_RE5887.mk
+++ twrp_RE5887.mk
@@ -10,6 +10,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
